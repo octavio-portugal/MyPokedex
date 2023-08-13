d5800f721c9c14077b010320a29ee3550b7f6ef4
Octavio Portugal <octaviolportugal@gmail.com>
2023-08-13 11:05:35 -0300

    first commit

---
+++ app/src/main/java/com/octavioportugal/mypokedex/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.octavioportugal.mypokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.octavioportugal.mypokedex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
